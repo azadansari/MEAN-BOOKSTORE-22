65ef1874d6905e74b5ac7b345f336f8e65ef5a55
azad ansari <azadansari7277@gmail.com>
2022-01-09 20:36:36 +0530

    added home and shared

--- book-store/book-store/src/app/app.module.ts
+++ book-store/book-store/src/app/app.module.ts
@@ -3,14 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { AuthModule } from './auth/auth/auth.module';
+import { HomeComponent } from './home/components/home/home.component';
+import { HomeModule } from './home/home.module';
+import { HeaderComponent } from './shared/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    AuthModule
+    AuthModule,
+    HomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
