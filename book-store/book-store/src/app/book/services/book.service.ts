694ba71412a1854e45d22100f36afae403d55805
azad ansari <azadansari7277@gmail.com>
2022-01-23 23:12:21 +0530

    cmnt

--- book-store/book-store/src/app/book/services/book.service.ts
+++ book-store/book-store/src/app/book/services/book.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { map } from 'rxjs/operators';
-import { BookModel } from '../models/book.model';
+// import { BookModel } from '../models/book.model';
 
 // Dependency Injection
 @Injectable({
